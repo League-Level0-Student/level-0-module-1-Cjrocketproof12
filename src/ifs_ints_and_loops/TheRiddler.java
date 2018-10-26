1e3cc54f0516388a1839aa0516efeb76af3892e3
league <league@wts-im-6.attlocal.net>
2018-10-25 17:00:44 -0700

    Riddler

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,16 +2,16 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int Score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+JOptionPane.showInputDialog("What Is That Thats Always Coming, But Never Arrives?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
@@ -19,7 +19,7 @@ public class TheRiddler {
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, "You Got "+Score);
 	}
 }
 
