501dcfc39b6a9afa467a48a93729ede9521b0da9
Cjrocketproof12 <league@iMac-6.local>
2018-12-11 16:55:08 -0800

    Tests

---
+++ src/ForloopTest.java
@@ -0,0 +1,34 @@
+
+public class ForloopTest {
+
+	public static void main(String[] args) {
+		
+	
+	
+	//1. Complete the for loop below so that it prints out the numbers 1­10.
+	//for( int i=0; i<111; i++ )
+	//{ 
+	//	System.out.println(i);
+	//}
+
+		
+		//for( int i=75; i>49; i-- )
+	//{
+	//	System.out.println(i);
+	//}	
+	//2. Complete the for loop below so that it counts down and prints out 75 to 50.
+	
+	
+		
+	//3. Describe briefly what each part of the for loop does: 
+	//for( a ; b ; c )
+	//{               } 
+	//a) Holds the numbers and is the beginning
+	//b) the number that it does up to or down to
+	//c) how much it goes up or down
+	
+	
+	
+	}
+	
+}
